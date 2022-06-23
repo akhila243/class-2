b3e159832c05aa03660f94d156cfd38d588ae163
akhila243 <107605821+akhila243@users.noreply.github.com>
2022-06-23 10:28:02 +0530

    Add files via upload

---
+++ slicing.py
@@ -0,0 +1,14 @@
+a='python'
+# print(a[:]) #python
+# print(a[2:]) #thon
+# print(a[:3]) #pyt
+# print(a[2:5]) #tho
+# print(a[2:4]) #th
+# print(a[4:5]) #o
+# print(a[1:7]) #ython
+# print(a[4:6]) #on
+# print(a[3:3]) #blankline
+# print(a[4:1]) #blankline
+# print(a[4:]) #on
+# print(a[:5]) #pytho
+# print(a[5:]) #n
